9b20872f517caffe21066f748ce5261eaa6662c9
404ibra <ibra.aktass@gmail.com>
2024-01-20 23:36:40 +0300

    project created

---
+++ NetflixUIKitClone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixUIKitClone
+//
+//  Created by İbrahim Aktaş on 19.01.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
